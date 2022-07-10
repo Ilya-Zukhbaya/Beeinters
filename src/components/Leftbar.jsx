4e419aefe34ee3639dc1f103dfad4abca80b077a
Ilya Zukhbaya <ne_perepletov@mail.ru>
2022-07-10 22:55:19 +0300

    Add changes in leftbar + switcher

--- src/components/Leftbar.jsx
+++ src/components/Leftbar.jsx
@@ -4,41 +4,45 @@ export const Leftbar = () => {
   return (
     <article className="leftbar__container">
       <div className="leftbar">
-        <div>
+        <div className="leftbar__first-line">
           <p>Программа</p>
-          <img src="./pictures/leftbar/arrowUp.png" alt="arrow up" />
+          <img src="./pictures/leftbar/arrowUp.png" alt="arrow up" className="leftbar__arrowUP" />
         </div>
         <ul>
           <li>Менторы</li>
           <li>
-            <div>
+            <div className="popup">
               <p>Лекции</p>
-              <img src="./pictures/leftbar/arrowUp.png" alt="arrow up" />
+              <img
+                src="./pictures/leftbar/arrowUp.png"
+                alt="arrow up"
+                className="leftbar_arrowUP"
+              />
             </div>
             <ul>
-              <li>
-                <p>HTML/CSS</p>
+              <li className="leftbar__sec-li">
                 <img src="./pictures/leftbar/1Shape.png" alt="sword" />
+                <p>HTML/CSS</p>
               </li>
-              <li>
-                <p>GIT</p>
+              <li className="leftbar__sec-li">
                 <img src="./pictures/leftbar/2Shape.png" alt="fireship" />
+                <p>GIT</p>
               </li>
-              <li>
-                <p>JavaScript</p>
+              <li className="leftbar__sec-li">
                 <img src="./pictures/leftbar/3Shape.png" alt="pazzle" />
+                <p>JavaScript</p>
               </li>
-              <li>
-                <p>React</p>
+              <li className="leftbar__sec-li">
                 <img src="./pictures/leftbar/4Shape.png" alt="rook" />
+                <p>React</p>
               </li>
-              <li>
-                <p>Angular</p>
+              <li className="leftbar__sec-li">
                 <img src="./pictures/leftbar/5Shape.png" alt="box" />
+                <p>Angular</p>
               </li>
-              <li>
-                <p>Data</p>
+              <li className="leftbar__sec-li">
                 <img src="./pictures/leftbar/6Shape.png" alt="ball" />
+                <p>Data</p>
               </li>
             </ul>
           </li>
@@ -49,6 +53,10 @@ export const Leftbar = () => {
       </div>
       <div className="leftbar__container-switcher">
         <p>Получать рассылку</p>
+        <label>
+          <input type="checkbox" name="recieve letters" />
+          <span></span>
+        </label>
       </div>
     </article>
   );
